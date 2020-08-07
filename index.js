485177be2c4342754cb29f2caee2b0ce8d1ce583
gabrielgog <gabrielgog96@gmail.com>
2020-08-07 06:39:53 +0000

    Initialize repository

---
+++ index.js
@@ -0,0 +1,28 @@
+// Reverses a string. 
+
+function reverse(string){
+   return string.split("").reverse().join("");
+
+}
+
+//Defines a phrase object.
+
+function Phrase(content){
+    this.content = content;
+
+    // Returns content processed for palindrome testing.
+
+    this.processedContent = function processedContent(){
+       return this.content.toLowerCase();
+    }
+
+
+ // Returns true for a palindrome, false otherwise. 
+
+
+   this.palindrome = function palindrome(){
+
+        return this. processedContent() === reverse(this.processedContent());
+    }
+}
+ 
